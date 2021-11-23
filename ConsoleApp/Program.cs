d4d1395ce20fa30887585d7130179efdb54d05b8
sheva <shevelyanchik01@mail.ru>
2021-11-24 01:19:23 +0300

    Implemented Tests Generator logic.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using TestGenerator;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private const string SourceFilePath1 =
+            "D:\\Study\\5SEM\\MPP\\MPP_LAB_4\\MPP_TEST_GENERATOR\\TestClassLib\\ClassExample1.cs";
+
+        private const string SourceFilePath2 =
+            "D:\\Study\\5SEM\\MPP\\MPP_LAB_4\\MPP_TEST_GENERATOR\\TestClassLib\\ClassExample3.cs";
+
+        private const string OutDirPath = "D:\\Study\\5SEM\\MPP\\MPP_LAB_4\\MPP_TEST_GENERATOR\\GenClasses";
+
+        private const int MaxParallelTasksCount = 3;
+
+
+        static void Main(string[] args)
+        {
+            var testsGenerator = new TestsGenerator(MaxParallelTasksCount);
+            testsGenerator.Generate(new List<string>() {SourceFilePath1, SourceFilePath2}, OutDirPath).Wait();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+﻿MPP_LAB_4 TESTS_GENERATOR
+>Сondition: [tests_generator_condition](https://bsuir.ishimko.me/mpp-dotnet/4-tests-generator)
